81429b86a7672953d478ade4c61e2b0da90c8347
Sameer Saini <38816886+soyo-kaze@users.noreply.github.com>
2020-10-09 15:20:24 +0000

    new

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from "react"
 import ReactDom from "react-dom"
 import { Button, Container, Col } from "react-bootstrap";
-import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "./node_modules/bootstrap/dist/css/bootstrap.min.css";
 import icon from "https://cdn.discordapp.com/attachments/747736205176733767/764042780867428362/posters1.png"
 
 const App = () =>{
