86f2918618cbc78bae21ca1d48d5e91a68095070
Aniket Kumar Sinha <sinhaaniket32@gmail.com>
2020-10-10 01:07:55 +0530

    checking

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <img src={Icon} alt="Hacktoberfest Logo" />
         <h1><i>Hacktoberfest Hackers</i></h1>
         <p>Name</p> { /* Copy paste this below and replace "Name" with your own name */ }
+        <p>Name2</p>
       </header>
     </div>
   );
