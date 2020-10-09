9e5edc775fa1c76e4058d6c10cb06878e428e3c1
Sameer Saini <38816886+soyo-kaze@users.noreply.github.com>
2020-10-09 15:57:40 +0000

    new

--- App.js
+++ App.js
@@ -2,14 +2,14 @@ import React from "react"
 import ReactDom from "react-dom"
 import { Button, Container, Col } from "react-bootstrap";
 import "./node_modules/bootstrap/dist/css/bootstrap.min.css";
-import icon from "https://cdn.discordapp.com/attachments/747736205176733767/764042780867428362/posters1.png"
+//import icon from "https://cdn.discordapp.com/attachments/747736205176733767/764042780867428362/posters1.png"
 
 const App = () =>{
     return(
         <>
             <Container style={{height:"500px",display:"flex",alignItems:"center"}}>
                 <Col className="text-center">
-                    <img src={icon} alt="Hacktoberfest Logo" />
+                    <img src="" alt="Hacktoberfest Logo" />
                     <h1><i>Hacktoberfest Hackers</i></h1>
                     <p>Name</p> 
                     { /* Copy paste this below and replace "Name" with your own name */ }
