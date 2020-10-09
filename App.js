3e939a1e017e35d124e84c4566fced9b2358be84
Sameer Saini <38816886+soyo-kaze@users.noreply.github.com>
2020-10-09 15:02:40 +0000

    new

--- App.js
+++ App.js
@@ -2,13 +2,14 @@ import React from "react"
 import ReactDom from "react-dom"
 import { Button, Container, Col } from "react-bootstrap";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import icon from "https://raw.githubusercontent.com/dscbkbiet/hacktoberfest/master/public/Icon.svg"
 
 const App = () =>{
     return(
         <>
             <Container style={{height:"500px",display:"flex",alignItems:"center"}}>
                 <Col className="text-center">
-                    <img src={"https://raw.githubusercontent.com/dscbkbiet/hacktoberfest/master/public/Icon.svg"} alt="Hacktoberfest Logo" />
+                    <img src={icon} alt="Hacktoberfest Logo" />
                     <h1><i>Hacktoberfest Hackers</i></h1>
                     <p>Name</p> 
                     { /* Copy paste this below and replace "Name" with your own name */ }
